bac1d3ee4a420b37ab9771aa82b585b777e78cdb
Francisco Javier Martínez Esparza <fmartineze@ciees.edu.mx>
2024-02-13 19:26:30 -0600

    Se creo la tabla de criptomonedas, se vió Axios y se creo el archivo css de App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from "react"
+import "./App.css"
+
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [criptos, setCriptos]= useState();
+
+  useEffect(() => {
+    console.log(`${API_URL}assets`)
+    fetch(`${API_URL}assets`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setCriptos(data.data);
+      })
+      .catch(() => {
+        console.error("La petición Falló");
+      })
+  }, []);
+
+  if (!criptos) return <span>Cargando...</span>
 
   return (
-    <h1>Hola mundo!!!!</h1>
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Ranking</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            criptos.map(({rank, name, priceUsd}) => (
+              <tr key={rank}>
+                <td>{rank}</td>
+                <td>{name}</td>
+                <td>{priceUsd}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <ol>
+      </ol>
+    </>
   )
 }
 
